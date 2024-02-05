6422964c3d38f9c689be55f8eb51c4fd223c36cf
Anthony Gicheru <anthonygicheru1@gmail.com>
2024-02-05 11:47:03 +0300

    understood arithmetic methods

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let num3 = 11;
+let num4 = 7;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 7, 6, 20);
